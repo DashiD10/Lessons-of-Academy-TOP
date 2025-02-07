f54d9cf7fe497c1f06e351596fb6c7f1a0b5602c
Dashi <dorzhiev_dd@mail.ru>
2025-02-08 11:43:08 +1200

    Глобал и перезапись глобальной переменной из функции

--- lesson_18.py
+++ lesson_18.py
@@ -4,16 +4,20 @@ a = 2
 
 def foo():
     a = 3
-    print(f' foo = {a}')
+    print(f'foo = {a}')
 
 def foo2():
+    global a
     a = 4
-    print(f' foo2 = {a}')
+    print(f'foo2 = {a}')
 
 def foo3():
-    print(f' foo3 = {a}')
+    print(f'foo3 = {a}')
           
 print(a)
 foo()
 foo2()
 foo3()
+print(a)
+
+

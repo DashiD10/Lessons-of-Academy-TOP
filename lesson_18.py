f45df9ab8a003d7d494e5c2a48f9556bd60bde78
Dashi <dorzhiev_dd@mail.ru>
2025-02-08 12:05:35 +1200

    Меняем переменную из подфункции в функции

--- lesson_18.py
+++ lesson_18.py
@@ -1,23 +1,43 @@
 # Функции. Области видимости. Замыкания. Декоратор. 
 
+# a = 2
+
+# def foo():
+#     a = 3
+#     print(f'foo = {a}')
+
+# def foo2():
+#     global a
+#     a = 4
+#     print(f'foo2 = {a}')
+
+# def foo3():
+#     print(f'foo3 = {a}')
+          
+# print(a)
+# foo()
+# foo2()
+# foo3()
+# print(a)
+
+
 a = 2
 
-def foo():
+def foo(): 
     a = 3
-    print(f'foo = {a}')
+    print(f'a после объявления переменной = {a}')
 
-def foo2():
-    global a
-    a = 4
-    print(f'foo2 = {a}')
+    def foo2():
+        nonlocal a   # Позволяет обратиться к переменной из внешней функции foo
+        a = 4
+        print(f'a после объявления переменной = {a}')
 
-def foo3():
-    print(f'foo3 = {a}')
-          
-print(a)
-foo()
-foo2()
-foo3()
-print(a)
+    foo2()
+    print(f'a после объявления переменной = {a}')
 
+foo() 
 
+# bananas = print
+
+# bananas("Привет!")
+# bananas("Как дела?")

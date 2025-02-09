7d729cf86709cf2012ab99569b28d8308ee331d7
Dashi <dorzhiev_dd@mail.ru>
2025-02-10 00:18:15 +1200

    еще коды

--- lesson_21.py
+++ lesson_21.py
@@ -3,20 +3,71 @@
 # Концепция ленивых вычислений
 # Сравнение range в списке и вне
 
+from pprint import pprint
 from typing import Generator
+from cities import cities_list
 
-START_VALUE = 0
-END_VALUE = 100
+print(len(cities_list))
+pprint(sorted(cities_list, key=lambda x: x['population'], reverse=True)[:5])
 
-range_obj = range(START_VALUE, END_VALUE)
+# Получим названия городов
 
-even_numbers = filter(lambda x: x % 2 == 0, range_obj)
 
-# Получаем map объект 
-even_numbers_str = map(lambda x: f'Число: {x}', even_numbers)
+# Классикаю Цикл.
+cities = []
+for city in cities_list:
+    cities.append(city['name'])
+print(cities)
 
-for num in even_numbers_str:
-    print(num)
+# Списквое выражение
+cities = [city['name'] for city in cities_list]
+
+# Генераторные функции
+
+def get_cities_gen(cities: list[dict]) -> Generator:
+    for city in cities:
+        yield city['name']
+
+cities_gen = get_cities_gen(cities_list)
+
+# Генераторные выражения
+cities_gen = (city['name'] for city in cities_list)
+
+# Поместим это в фильтр - нужны города, начинающиеся на й
+filtered_cities = (city['name'] for city in cities_list if city['name'].startswith('Ш'))
+
+for city in filtered_cities:
+    print(city)
+
+# в ленивом режиме  
+file_name = 'cities.txt'
+
+with open(file_name, 'w', encoding='utf-8') as f:
+    for city in cities_gen:
+        f.write(f'{city}\n')
+
+# Построяное чтение - возвращаем генератор
+# with open(file_name, 'r', encoding='utf-8') as f:
+#     # for line in f:
+#     #     print(line.strip())
+#     cities = (line.strip() for line in f)
+
+# Получим города где есть тире
+    # filtered_cities = (city for city in cities if "-" in city)
+
+def read_file_lines(filename: str) -> Generator[str]:
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            yield line.strip()
+
+cities = read_file_lines(file_name)
+
+for city in cities:
+    print(city)
+
+
+
+
 
 
 

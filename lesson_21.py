1291c34f69e54ba9f38ab670c3569f64812c1974
Dashi <dorzhiev_dd@mail.ru>
2025-02-09 23:21:13 +1200

    цепочка генераторов

--- lesson_21.py
+++ lesson_21.py
@@ -8,41 +8,17 @@ from typing import Generator
 START_VALUE = 0
 END_VALUE = 100
 
-# list_range = list(range(START_VALUE, END_VALUE))
+range_obj = range(START_VALUE, END_VALUE)
 
-# range_object = range(START_VALUE, END_VALUE)
-# print(range_object, type(range_object))
+even_numbers = filter(lambda x: x % 2 == 0, range_obj)
 
-# for num in range_object:
-#     print(num)
+# Получаем map объект 
+even_numbers_str = map(lambda x: f'Число: {x}', even_numbers)
 
-potatos = ["картошка", "картошка", "гнилая картошка", "картошка", "гнилая картошка", "картошка"]
+for num in even_numbers_str:
+    print(num)
 
-def clean_potato(potatos):
-    new_potatos = []
-    for potato in potatos:
-        potato += "_чищенная"
-        new_potatos.append(potato)
-    return new_potatos
 
-cleaned_potatos = clean_potato(potatos)
-print(cleaned_potatos)
 
-# Генераторные функции. Yield
 
-def generator_clean_potatos(potatos: list[str]) -> Generator[str]:
-    for potato in potatos:
-        potato += "_чищенная"
-        yield potato
-
-cleaned_potatos = generator_clean_potatos(potatos)
-print(next(cleaned_potatos))
-print(next(cleaned_potatos))
-print(next(cleaned_potatos))
-
-for potato in cleaned_potatos:
-    print(potato, "Из цикла")
-
-for potato in generator_clean_potatos(potatos):
-    print(potato, "Из цикла_2")
 

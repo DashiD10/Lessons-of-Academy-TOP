db25c769b8fe9d0bad18afc4c1f23c2998ce0bbb
Dashi <dorzhiev_dd@mail.ru>
2025-02-15 22:43:22 +1200

    создание атрибутов вне класса

--- lesson_22.py
+++ lesson_22.py
@@ -5,3 +5,8 @@ p1 = Person()
 p2 = Person()
 
 print(p1, p2) #<__main__.Person object at 0x0000021D9C906A50> <__main__.Person object at 0x0000021D9CB7CCD0>
+
+p1.name = "Филлип"
+p2.name = "Джордж"
+
+print(p1.name, p2.name) #Филлип Джордж

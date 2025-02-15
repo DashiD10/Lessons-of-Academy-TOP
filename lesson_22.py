2081965b67e5e01b0bd3c91f6e78b865b096c2ca
Dashi <dorzhiev_dd@mail.ru>
2025-02-15 23:23:35 +1200

    методы и атрибуты экземпляра

--- lesson_22.py
+++ lesson_22.py
@@ -1,13 +1,16 @@
 class Person:
-    # атрибут класса
-    name = "Фриц"
+    def __init__(self, name: str):
+        self.name = name
 
-p1 = Person()
-p2 = Person()
+    def say_my_name(self):
+        print(f'Меня зовут {self.name}')
 
-print(p1.name, p2.name) #<__main__.Person object at 0x0000021D9C906A50> <__main__.Person object at 0x0000021D9CB7CCD0>
+p1 = Person("Барак")
+p2 = Person("Владимир")
+p3 = Person("Дональд")
 
-p1.name = "Филлип"
-p2.name = "Джордж"
+print(p1.name, p2.name, p3.name) #<__main__.Person object at 0x0000021D9C906A50> <__main__.Person object at 0x0000021D9CB7CCD0>
+p1.say_my_name()
+p1.say_my_name()
+p1.say_my_name()
 
-print(p1.name, p2.name) #Филлип Джордж

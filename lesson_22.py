5ddef3b5921ab7ed86505aad013e75208d3428ff
Dashi <dorzhiev_dd@mail.ru>
2025-02-16 00:32:42 +1200

    Еще коммит

--- lesson_22.py
+++ lesson_22.py
@@ -1,16 +1,31 @@
-class Person:
-    def __init__(self, name: str):
-        self.name = name
+class AdPost:
+    promote_rate: float = 0.5
 
-    def say_my_name(self):
-        print(f'Меня зовут {self.name}')
+    def __init__(self, title: str, text: str, price: int):
+        self.title = title
+        self.text = text
+        self.price = price
+        # self.promote_rate: float = 0.5
 
-p1 = Person("Барак")
-p2 = Person("Владимир")
-p3 = Person("Дональд")
+    def __str__(self) -> str:
+        return f"Заголовок: {self.title}, Текст: {self.text[20]}, Цена: {self.price}"
 
-print(p1.name, p2.name, p3.name) #<__main__.Person object at 0x0000021D9C906A50> <__main__.Person object at 0x0000021D9CB7CCD0>
-p1.say_my_name()
-p1.say_my_name()
-p1.say_my_name()
+    def calculate_promote_cost(self, day: int) -> int:
+        promote_cost = int(self.price * (self.promote_rate / 100) * day)
+        return promote_cost
 
+    @staticmethod
+    def get_peak_hours() -> tuple:
+        return 13, 14, 15
+
+
+ap1 = AdPost("Продам ноутбук", "Продам ноутбук в отличном состоянии", 10000)
+ap2 = AdPost("Куплю ноутбук", "Куплю ноутбук в отличном состоянии", 10000)
+
+AdPost.promote_rate = 1
+
+print(ap1)
+print(ap2)
+print(ap1.calculate_promote_cost(10))
+print(ap2.calculate_promote_cost(10))
+print(AdPost.get_peak_hours())

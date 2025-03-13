71ee6cb15d180340c6a05210f1f86c9550b36812
Dashi <dorzhiev_dd@mail.ru>
2025-03-13 19:52:15 +1200

    инкапсуляция

--- lesson_24.py
+++ lesson_24.py
@@ -5,6 +5,7 @@ _ это protected - доступно при наследовании
 __ это private - доступно внутри класса
 """
 
+
 class Car:
     def __init__(self, model: str, color: str):
         self.model = model
@@ -12,15 +13,34 @@ class Car:
         self._speed = 0
         self.__max_speed = 200
 
-    def __str__(self)
-        return f"Марка {self.model}, максимальная скорость {self.__max_speed}" 
-       
-volga = Car("Volga", "black")
-print(volga.model)
-print(volga.color)
-print(volga._speed)
-# print(volga.__max_speed)
-print(volga._Car__max_speed)
-#AttributeError: 'Car' object has no attribute '__max_speed'. Did you mean: '_Car__max_speed'?
-# Шаблон искажения имени..
+    def __str__(self):
+        return f"Марка {self.model}, максимальная скорость {self.__max_speed}"
 
+    def __validate_speed(self, speed: int):
+        if speed < 0:
+            raise ValueError("Скорость не может быть меньше 0")
+
+        if speed > self.__max_speed:
+            raise ValueError("Скорость не может быть больше максимальной")
+
+    def set_speed(self, speed: int):
+        self.__validate_speed(speed)
+        self._speed = speed
+
+
+class Driver:
+    def __init__(self, name: str, car: Car):
+        self.name = name
+        self.car = car
+
+    def drive(self, speed: int):
+        print(f"{self.name} сел в {self.car.model}")
+        self.car.set_speed(speed)
+        print(f"Скорость {self.car.model} {self.car._speed}")
+
+
+volga = Car("Volga", "black")
+driver = Driver("Ivan", volga)
+driver.drive(100)
+driver.drive(200)
+driver.drive(300)
